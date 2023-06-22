05c22f7450c69545629aad535981b5a61729ef6f
Anusha B Nadageri <104756174+anushN07@users.noreply.github.com>
2023-06-23 03:26:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+document.addEventListener('DOMContentLoaded', function() {
+  const sliderItems = document.querySelectorAll('#photos .slider-item');
+  const fullImageOverlay = document.querySelector('#photos .full-image-overlay');
+  const fullImage = document.querySelector('#photos .full-image-overlay img');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+  sliderItems.forEach((item) => {
+    item.addEventListener('click', function() {
+      const imageSrc = this.querySelector('img').getAttribute('src');
+      fullImage.src = imageSrc;
+      fullImageOverlay.classList.add('active'); // Add the 'active' class to display the overlay
+    });
+  });
+
+  fullImageOverlay.addEventListener('click', function(e) {
+    if (e.target === fullImageOverlay) {
+      fullImageOverlay.classList.remove('active'); // Remove the 'active' class to hide the overlay
+    }
+  });
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
